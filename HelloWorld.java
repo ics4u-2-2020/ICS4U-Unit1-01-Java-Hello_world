3349e57b58dee70532284010bfd1a223271bf9d8
Ubuntu <ubuntu@ip-172-31-47-160.ec2.internal>
2021-04-21 17:39:21 +0000

    Java program

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+* The Hello World program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  Cameron Teed
+* @version 1.0
+* @since   2020-04-21
+*/
+final class HelloWorld {
+    private HelloWorld() {
+        // Prevent instantiation
+        // Optional: throw an exception e.g. AssertionError
+        // if this ever *is* called
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+  /**
+  * This method prints out "Hello, World!".
+  * @param args
+  */
+  public static void main(final String[] args) {
+        System.out.println("Hello World");
+  }
+}
